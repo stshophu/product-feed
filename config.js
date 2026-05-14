62cfa3a1bdfd48479930ab59d355c6c759032aea
Kitti <kittili@MacBook-Pro-von-Kitti.local>
2026-05-14 21:57:02 +0200

    Remove 3170 Warehouse - overlaps with supplier feed

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
   locations: {
-    "3171 Warehouse": { markup: 0.26 },
-    "3170 Warehouse": { markup: 0.26 },
-    "RewixSync":      { markup: 0.26 },
+    "3171 Warehouse": { markup: 0, shipping: 15 },
+    "RewixSync":      { markup: 0, shipping: 15 },
   },
   shopifyApiVersion: "2025-01",
   defaultLocale: "nl_NL",
