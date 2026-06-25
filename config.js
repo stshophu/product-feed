f02196d327ed610dfe3ced445c313ad988da00ee
stshophu <info@siebentaschen.com>
2026-06-25 15:35:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   locations: {
     "3171 Warehouse": { markup: 0, shipping: 15 },
     "3140 Warehouse": { markup: 0, shipping: 10, costBased: true },
+    "3170 Warehouse": { markup: 0, shipping: 15, costBased: true },
   },
   shopifyApiVersion: "2025-01",
   defaultLocale: "nl_NL",
